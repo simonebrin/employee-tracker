3772c85a48d8bf6289428c6267c56d247459c485
Simone Brin <simonebrin@gmail.com>
2022-10-19 10:57:14 -0400

    added more menu questions

--- connection/connection.js
+++ connection/connection.js
@@ -1,4 +1,5 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
 
 // Connect to database
 const db = mysql.createConnection(
--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-import inquirer from "inquirer";
-import db from "./connection/connection.js";
-import CTable from "console.table";
+import inquirer from 'inquirer';
+import db from './connection/connection.js';
+import CTable from 'console.table';
 
-const menuQuestions = [
+const mainMenuQuestions = [
     {
-    type: "list",
-    name: "answer",
-    message: "What would you like to do?",
-    choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee"],
+    type: 'list',
+    name: 'answer',
+    message: 'What would you like to do?',
+    choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee'],
     },
 ]
 
 function menuQuestion() {
-    inquirer.prompt(menuQuestions).then((answers) => {
+    inquirer.prompt(mainMenuQuestions).then((answers) => {
         console.log(answers);
-        if (answers.menuQuestions === "View all departments") {
+        if (answers.mainMenuQuestions === 'View all departments') {
             
         }
     })
@@ -23,9 +23,35 @@ function menuQuestion() {
 // menuQuestion();
 db.promise()
   .query(
-    "SELECT roles.title, roles.salary, departments.department_name as department FROM roles LEFT JOIN departments on roles.department_id=departments.id;"
+    'SELECT roles.title, roles.salary, departments.department_name as department FROM roles LEFT JOIN departments on roles.department_id=departments.id;'
   )
   .then((roles) => {
     console.table(roles[0]);
   });
 
+const addDepartmentQuestion = [
+    {
+        type: 'input',
+        name: 'department',
+        message: 'Add the department name'
+    }
+]
+
+const addRoleQuestions = [
+    {
+        type: 'input',
+        name: 'role',
+        message: 'Input role:'
+    },
+    {
+        type: 'input',
+        name: 'salary',
+        message: 'Enter new role salary:'
+    },
+    {
+        type: 'list',
+        name: 'departments',
+        message: 'Select your department:',
+        choices: 
+    }
+]
